6abd11a72863dad4f7eaf94179db3e05850ccf81
jovan <jovan95vujicic@gmail.com>
2019-03-22 08:33:53 +0100

    bug and errors fix

--- calorie-calculator-react/src/components/forms/LoginForm.js
+++ calorie-calculator-react/src/components/forms/LoginForm.js
@@ -34,7 +34,7 @@ class LoginForm extends React.Component {
       const { data,errors } = this.state;
       return (
         <Form onSubmit = {this.onSubmit}>
-          <Form.Field>
+          <Form.Field error={!!errors.email}>
             <label htmlFor='email'>Email</label>
             <input
               type='email'
@@ -46,7 +46,7 @@ class LoginForm extends React.Component {
               />
               {errors.email && <InlineError text={errors.email} />}
           </Form.Field>
-          <Form.Field>
+          <Form.Field error={!!errors.password}>
             <label htmlFor='password'>Email</label>
             <input
               type='password'
