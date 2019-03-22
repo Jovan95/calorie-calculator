be7c58c058c47e0ea67fa5a7dae782eca84b4c02
jovan <jovan95vujicic@gmail.com>
2019-03-22 08:51:50 +0100

    client-side validation and submit

--- calorie-calculator-react/src/components/forms/LoginForm.js
+++ calorie-calculator-react/src/components/forms/LoginForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Form,Button } from 'semantic-ui-react';
 import Validator from 'validator';
 import InlineError from "../messages/InlineError";
+import PropTypes from 'prop-types';
 
 class LoginForm extends React.Component {
     state = {
@@ -21,6 +22,9 @@ class LoginForm extends React.Component {
     onSubmit = () => {
       const errors = this.validate(this.state.data);
       this.setState({ errors });
+      if (Object.keys(errors).length === 0) {
+        this.props.submit(this.state.data)
+      }
     };
 
     validate = data => {
@@ -63,5 +67,9 @@ class LoginForm extends React.Component {
     )
   }
 }
+
+LoginForm.propTypes = {
+  submit: PropTypes.func.isRequired
+};
 
 export default LoginForm;
