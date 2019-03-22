52a52025964b76485d2476a4d6b5b2ea7813a35d
jovan <jovan95vujicic@gmail.com>
2019-03-22 08:12:06 +0100

    Form, prop-types and inline errors

--- calorie-calculator-react/src/components/pages/LoginPage.js
+++ calorie-calculator-react/src/components/pages/LoginPage.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React from 'react';
+import LoginForm from '../forms/LoginForm';
 
-const
+const LoginPage = () => (
+  <div>  
+    <h1>Login page</h1>
+    <LoginForm />
+  </div>
+);
 
-
-export default LoginPage
+export default LoginPage;
