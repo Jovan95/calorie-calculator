f0f2ca045929c60a0d5d6791959d7cdd5cd400f0
jovan <jovan95vujicic@gmail.com>
2019-03-23 08:26:01 +0100

    Redux setup

--- calorie-calculator-react/src/index.js
+++ calorie-calculator-react/src/index.js
@@ -3,11 +3,23 @@ import "semantic-ui-css/semantic.min.css";
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { createStore, applyMiddleware} from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import * as serviceWorker from './serviceWorker';
+import rootReducer from './rootReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thnuk))
+);
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
    document.getElementById('root')
 );
