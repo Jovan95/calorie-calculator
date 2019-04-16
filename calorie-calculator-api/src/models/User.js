e2f1cd0ae1939acc76d4737ae83df107d333010e
jovan <jovan95vujicic@gmail.com>
2019-04-16 11:56:17 +0200

    added JSON web token

--- calorie-calculator-api/src/models/User.js
+++ calorie-calculator-api/src/models/User.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
 
 const schema = new mongoose.Schema(
   {
@@ -11,6 +13,21 @@ const schema = new mongoose.Schema(
 
 schema.methods.isValidPassword = function isValidPassword(password) {
   return bcrypt.compareSync(password, this.passwordHash)
+};
+
+schema.methods.generateJWT = function generateJWT() {
+  return jwt.sign(
+    {
+    email: this.email
+    },
+    "secretkey")
+};
+
+schema.methods.toAuthJSON = function toAuthJSON() {
+  return {
+    email: this.email,
+    token: this.generateJWT()
+  }
 };
 
 export default mongoose.model('User', schema);
