6978f57f4244b775b3e3979592ec8d9f808281e7
jovan <jovan95vujicic@gmail.com>
2019-04-18 18:20:00 +0200

    Added singup route that creates new user and encrypts password and saves it in password hash in database.It handels api post calls from sing up form .

--- calorie-calculator-api/src/models/User.js
+++ calorie-calculator-api/src/models/User.js
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import uniqueValidator from 'mongoose-unique-validator';
 
 
 const schema = new mongoose.Schema(
   {
-  email : {type: String, required: true, lowercase: true, index: true},
+  email : {type: String, required: true, lowercase: true, index: true, unique: true},
   passwordHash: {type: String, required: true}
   },
   { timestamps: true}
@@ -14,6 +15,10 @@ const schema = new mongoose.Schema(
 schema.methods.isValidPassword = function isValidPassword(password) {
   return bcrypt.compareSync(password, this.passwordHash)
 };
+
+schema.methods.setPassword = function setPassword(password) {
+  this.passwordHash = bcrypt.hashSync(password, 10);
+}
 
 schema.methods.generateJWT = function generateJWT() {
   return jwt.sign(
@@ -29,5 +34,7 @@ schema.methods.toAuthJSON = function toAuthJSON() {
     token: this.generateJWT()
   }
 };
+
+schema.plugin(uniqueValidator, {message: "This email is already taken!"});
 
 export default mongoose.model('User', schema);
--- calorie-calculator-react/src/api.js
+++ calorie-calculator-react/src/api.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 export default {
   user: {
     login: credentials =>
-      axios.post('/api/auth', { credentials }).then(res => res.data.user)
+      axios.post('/api/auth', { credentials }).then(res => res.data.user),
+    singup: credentials =>
+      axios.post('/api/singup', { credentials }).then(res => res.data.user)
   }
 };
